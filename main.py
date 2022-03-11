0770f6d8cba881cd30f3d20e26b2f63facf5204b
Илья <i.bychkov.i@mail.ru>
2022-03-11 22:57:38 +0300

    Форма регистрации

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from datetime import datetime as dt
+
 from flask import Flask, render_template, redirect
+
 from data import db_session
-from data.users import User
 from data.jobs import Jobs
+from data.users import User
+from forms.user_form import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -57,6 +60,37 @@ def index():
         leader = db_sess.query(User).filter(User.id == job.team_leader).first()
         leaders.append(leader.name + ' ' + leader.surname)
     return render_template("index.html", jobs=jobs, leaders=leaders)
+
+
+@app.route("/login")
+def login():
+    return "All good!"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', form=form, message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
